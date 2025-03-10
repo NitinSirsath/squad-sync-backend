c0b6557362a46d1c4746bd95e4b309a4532e8a48
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-03-10 16:23:02 +0530

    added logout api

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -4,11 +4,12 @@ import {
   getLogout,
   getRegister,
 } from "./controllers/auth.controllers.ts";
+import { authenticateToken } from "../../middleware/authenticateToken.ts";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/login", getLogin);
 authRoutes.post("/register", getRegister);
-authRoutes.post("/logout", getLogout);
+authRoutes.post("/logout", authenticateToken, getLogout);
 
 export default authRoutes;
